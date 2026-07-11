424b48f4a7d54c1189b13062796a365b1fe608e6
reyyksy <reihankaissya2009@gmail.com>
2026-07-11 13:36:33 +0700

    fix delete 'style.css'

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('.link-btn');
-
-  buttons.forEach((button) => {
-    button.classList.add('is-ready');
-  });
-});
